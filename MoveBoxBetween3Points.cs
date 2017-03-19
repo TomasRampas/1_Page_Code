ef29849b6435c8d0635953051b8ee6f336862c91
TomasRampas <rampas.tomas@gmail.com>
2017-03-19 16:58:41 +0100

    Movement and rotation of player box
    This two scripts show how to move player between three points, or rotate
    by set degrees

---
+++ MoveBoxBetween3Points.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Move cube between three set points by pressing A and D keys.
+ * Cube moves smoothly.
+ * Inspired by endless runner games, where player just changes
+ * paths by swiping from left to right.
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PujdeTo : MonoBehaviour {
+
+    public bool moveBox = true;
+    private float objectSpeed = 4;
+
+    [SerializeField] Vector3 Point0;
+    [SerializeField] Vector3 Point1;
+    [SerializeField] Vector3 Point2;
+
+    Vector3[] PointList = new Vector3[3];
+
+    private int selectedPoint = 1;
+
+    private Vector3 movementDirection;
+    private Vector3 pointPosition;
+
+    void Start ()
+    {
+        PointList[0] = Point0;
+        PointList[1] = Point1;
+        PointList[2] = Point2;
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AKeyClicked();
+            Debug.Log("A key pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DKeyClicked();
+            Debug.Log("D key pressed");
+        }
+
+        if (!moveBox)
+        {
+            return;
+        }
+
+        transform.Translate(movementDirection * (objectSpeed * Time.deltaTime));
+
+        if (Mathf.Abs((pointPosition - transform.localPosition).x) <= 0.20f)
+        {
+            moveBox = false;
+        }
+    }
+
+    void AKeyClicked()
+    {
+        if (selectedPoint == 0)
+        {
+            return;
+        }
+
+        if (selectedPoint >= 1)
+        {
+            movementDirection = Vector3.left;
+            selectedPoint -= 1;
+            pointPosition = PointList[selectedPoint];
+            moveBox = true;
+            Debug.Log("selectedPoint" + selectedPoint);
+        }
+    }
+
+    void DKeyClicked()
+    {
+        if (selectedPoint == 2)
+        {
+            return;
+        }
+
+        if (selectedPoint <= 1)
+        {
+            movementDirection = Vector3.right;
+            selectedPoint += 1;
+            pointPosition = PointList[selectedPoint];
+            moveBox = true;
+            Debug.Log("selectedPoint" + selectedPoint);
+        }
+    }
+
+}
---
+++ RotateBoxOnKey.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * This code explains how to rotate a cube by pressing left and right arrows
+ * The rotation is always done by 90 degrees each time
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    public float smooth = 1f;
+    private Quaternion targetRotation;
+
+	void Start () {
+        targetRotation = transform.rotation;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown ("left"))
+        {
+            targetRotation *= Quaternion.AngleAxis(90, Vector3.forward);
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            targetRotation *= Quaternion.AngleAxis(90, Vector3.back);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+	}
+}
