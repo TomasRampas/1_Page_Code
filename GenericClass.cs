1139bc9df30e533dc8b96c390d97e92656e5beb2
TomasRampas <rampas.tomas@gmail.com>
2017-03-22 12:46:10 +0100

    Generic class

---
+++ GenericClass.cs
@@ -0,0 +1,36 @@
+﻿// Example of a Generic class usage
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+public GameObject GameManager2;
+
+void Awake () 
+{
+	KeyValuePair<int, string> course = new KeyValuePair<int, string> (1, "Devslopes");
+	course.Print();
+
+	KeyValuePair<string, string> lesson = new KeyValuePair<string, string> ("Number 1 Lesson", "Learning about generics");
+	lesson.Print();
+}
+
+public class KeyValuePair<Tkey, Tvalue>
+{
+	public Tkey key;
+	public Tvalue value;
+
+	public KeyValuePair(Tkey _key, Tvalue _value)
+	{
+		key = _key;
+		value = _value;
+	}
+
+	public void Print()
+	{
+		Debug.Log("Key: " + key.ToString());
+		Debug.Log("Value: " + value.ToString());
+	}
+}
